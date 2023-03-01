2b58b635919c3459186e8dccf72a1eba0dce53f5
Naimur Reza <naimur.rezaa@gmail.com>
2023-03-01 21:44:32 +0600

    done upto click to show image

--- js/script.js
+++ js/script.js
@@ -23,4 +23,27 @@ const displayCatagoriesNews = (data, category_name) => {
  const catName = document.getElementById('category-name')
  count.innerText = data.length;
  catName.innerText = category_name;
+
+ //adding news card to the all news section
+ const newsContainer = document.getElementById('all-news');
+ newsContainer.innerHTML = '';
+ data.forEach(newses => {
+    newsContainer.innerHTML += `
+    <div class="card mb-3 container my-4">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${newses.image_url}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${newses.title}</h5>
+        <p class="card-text">${newses.details.slice(1,250)}</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+    `
+    console.log(newses);
+ })
 };
