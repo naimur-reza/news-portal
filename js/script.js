31e4d5e9ab1c570437e8e3fde2a0a821247032f0
Naimur Reza <naimur.rezaa@gmail.com>
2023-03-01 21:15:16 +0600

    done upto counter value and cat name

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
 const loadData = () => {
-    fetch('https://openapi.programming-hero.com/api/news/categories')
-    .then(res => res.json())
-    .then(data => showCatagories(data.data.news_category))
-}
-const showCatagories = (data) =>{
-    const container = document.getElementById('container');
-    //step-1 : Add element to the container.
-    data.forEach(element => {
-        container.innerHTML += ` <a class="nav-link" onclick="fetchCatagoriesNews('${element.category_id}')" href="#">${element.category_name}</a>`
-    });
-}
-
+  fetch("https://openapi.programming-hero.com/api/news/categories")
+    .then((res) => res.json())
+    .then((data) => showCatagories(data.data.news_category));
+};
+const showCatagories = (data) => {
+  const container = document.getElementById("container");
+  //step-1 : Add element to the container.
+  data.forEach((element) => {
+    container.innerHTML += ` <a class="nav-link" onclick="fetchCatagoriesNews('${element.category_id}','${element.category_name}')" href="#">${element.category_name}</a>`;
+  });
+};
 
 //fetch all newses
-const fetchCatagoriesNews = (id) =>{
-    const URL = `https://openapi.programming-hero.com/api/news/category/${id}`
-    fetch(URL)
-    .then(res => res.json())
-    .then(data => displayCatagoriesNews(data.data))
-    console.log(URL);
-}
-const displayCatagoriesNews = (data) => {
-    console.log(data);
-}
+const fetchCatagoriesNews = (id, category_name) => {
+  const URL = `https://openapi.programming-hero.com/api/news/category/${id}`;
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => displayCatagoriesNews(data.data, category_name));
+};
+const displayCatagoriesNews = (data, category_name) => {
+ const count = document.getElementById('news-count');
+ const catName = document.getElementById('category-name')
+ count.innerText = data.length;
+ catName.innerText = category_name;
+};
